0bdd4b616d431860dc42464dddab6f4892a85513
Niklas <138971666+keimschleuder@users.noreply.github.com>
2024-03-22 12:55:55 +0100

    Worked on the Game

--- Game.py
+++ Game.py
@@ -1,14 +1,45 @@
 from enum import Enum
+import random
+import Intro
 
 doAmor = True
 
+werewolves = [1, 1, 2, 2, 2, 3, 3, 4, 4]
+
+def RoleCount(playersCount):
+    count = []
+    if werewolves[playersCount - 8] >= 3: # Weisser Wolf
+        count.append(1)
+    else:
+        count.append(None)
+    count.append(werewolves[playersCount - 8]) # Werevolves
+    count.append(-1) # Dorfbewohner
+    # Hexe, Seherin, Amor, Jaeger und Rabe
+    for _ in range(5):
+        count.append(1)
+
+def AssignRoles(players: list):
+    num = len(players)
+    count = RoleCount(num)
+    playersNew = []
+    for _ in range(num):
+        rand = random.randint(0, len(players) - 1)
+        playersNew.append(players[rand])
+        players.pop(rand)
+
+    # TODO: Assign Roles
+
+    return playersNew
+
 class Roles(Enum):
-    WERWOLF = 0
-    DORFBEWOHNER = 1
-    HEXE = 2
-    SEHERIN = 3
-    AMOR = 4
-    JAEGER = 5
+    WEISSER_WOLF = 0
+    WERWOLF = 1
+    DORFBEWOHNER = 2
+    HEXE = 3
+    SEHERIN = 4
+    AMOR = 5
+    JAEGER = 6
+    RABE = 7
 
 class Cycle(Enum):
     DAY = 0
@@ -25,27 +56,31 @@ class PlayerState(Enum):
     EVIL_LOVER = 4
 
 class Player():
-    def __init__(self, id, role: Roles) -> None:
+    def __init__(self, id) -> None:
         self.playerID = id
         self.inLove = False
         self.loverIsOpponentTeam = False
+    
+    def setRole(self, role):
         self.role = role
-        if self.role >= 1:
-            self.playerState = PlayerState.GOOD
+        if self.role >= 2:
+            self.playerState = PlayerState.GOOD.value
         else:
-            self.playerState = PlayerState.EVIL
+            self.playerState = PlayerState.EVIL.value
 
 class Game():
     def __init__(self) -> None:
-        self.state = Cycle.AMOR
+        self.state = Cycle.AMOR.value
         self.players = []
+        self.lovers = []
+        self.hauptmann = None
 
     def nextCycle(self):
         self.state = self.state + 1
-        if self.state == Cycle.AMOR and not doAmor:
+        if self.state == Cycle.AMOR.value and not doAmor:
             self.state = self.state + 1
         if self.state == 5:
-            self.state = Cycle.DAY
+            self.state = Cycle.DAY.value
 
     def Abstimmung(self):
         pass
---
+++ Intro.py
@@ -0,0 +1,11 @@
+story = [
+    "Tief im Düsterwald liegt ein kleines Dorf.", 
+    "Es hat keinerlei Kontakt zum Rest der Welt, denn niemand außer den Dorfbewohnern traut sich, mehr als den Rand des Waldes zu erforschen.", 
+    "Dafür gibt es einen guten Grund: Im Düsterwald lauern finstere, blutrünstige Kreaturen: die Werwölfe.",
+    "Bei Tag sind sie ganz normale Menschen, doch in der Nacht verwandeln sie sich.",
+    "Sie nehmen ein wölfisches Aussehen an, werden so stark wie mehrere erwachsene Männer zusammen und fressen jeden Menschen, der ihnen begegnet.",
+    "Aber auch die Dorfbewohner sind nicht ganz wehrlos:",
+    "Über die vielen Jahre des Kampfes gegen die Werwölfe haben manche von ihnen besondere Fähigkeiten entwickelt.",
+    "Es ist also noch nicht entschieden, ob Menschen oder Werwölfe den Kampf um den Düsterwald gewinnen werden.",
+    "Auf welcher Seite stehst du?"
+]
